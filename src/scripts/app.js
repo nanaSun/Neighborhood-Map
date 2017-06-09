4697e3977cebaaa4314982a07f8a693c07738405
cherryvenus <sunmeiye@163.com>
2017-06-09 10:31:27 +0800

    fix some problem

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,22 +1,22 @@
 var map,
-	centerPoint={lat: 37.77493,lng: -122.41942},//initial center point
-	places=[],
-	infoWindow,
-	service,
-	request,
-    init=false;
+    centerPoint = { lat: 37.77493, lng: -122.41942 }, //initial center point
+    places = [],
+    infoWindow,
+    service,
+    request,
+    init = false;
 //after google map api loaded, run this function
 function initMap() {
-	//init map
+    //init map
     map = new google.maps.Map(document.getElementById('map'), {
-      center:centerPoint,
-      zoom:13,
-      zoomControl: true,
-	  mapTypeControl: false,
-	  scaleControl: true,
-	  streetViewControl: true,
-	  rotateControl: true,
-	  fullscreenControl: false
+        center: centerPoint,
+        zoom: 13,
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: true,
+        streetViewControl: true,
+        rotateControl: true,
+        fullscreenControl: false
     });
     //init infoWindow
     infoWindow = new google.maps.InfoWindow();
@@ -26,25 +26,25 @@ function initMap() {
     //marker animation
     function markerBounce(marker) {
         marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function(){
+        setTimeout(function() {
             marker.setAnimation(null);
-        },1400);
+        }, 1400);
     }
 
     //place detail window
-    function infoPanel(data){
-        return "<img src='"+data.image_url+"' width='40px'/>"+"<br/>"+(!data.isclose?"opening":"closed")+"<br/>"+"phone: " +data.phone+"<br/>"+data.name+"<br/>"+data.location.display_address.join(",")+"<br/>";
+    function infoPanel(data) {
+        return "<img src='" + data.image_url + "' width='40px'/>" + "<br/>" + (!data.isclose ? "opening" : "closed") + "<br/>" + "phone: " + data.phone + "<br/>" + data.name + "<br/>" + data.location.display_address.join(",") + "<br/>";
     }
 
     //each place function
-    function place(data,map) {
+    function placeItem(data, map) {
         var _ = this;
-        _.data=data;
+        _.data = data;
         _.marker = new google.maps.Marker({
             map: map,
             position: new google.maps.LatLng(_.data.coordinates.latitude, _.data.coordinates.longitude)
         });
-        _.showDetail=function(){
+        _.showDetail = function() {
             map.setCenter(new google.maps.LatLng(_.data.coordinates.latitude, _.data.coordinates.longitude));
             map.setZoom(15);
             infoWindow.close();
@@ -53,91 +53,91 @@ function initMap() {
         };
 
         //show info with animation
-        _.clickMarkerDetail=function(){
+        _.clickMarkerDetail = function() {
             markerBounce(_.marker);
             _.showDetail();
         };
         google.maps.event.addListener(_.marker, 'click', _.clickMarkerDetail);
 
         //clear marker on the map
-        _.clear=function(){
-            _.marker.setMap(null);
+        _.clear = function() {
+            _.marker.setVisible(false);
         };
-        _.show=function(){
-            _.marker.setMap(map);
+        _.show = function() {
+            _.marker.setVisible(true);
         };
         _.show();
     }
 
     //search place service
-    function searchPlace(request,callback){
+    function searchPlace(request, callback) {
         return $.ajax({
             url: "http://localhost:8888",
-            data:request,
+            data: request,
             type: 'POST',
             success: function(data) {
-                if("error" in data){
+                if ("error" in data) {
                     alert("system error");
                     return;
                 }
                 callback(data);
             },
-            fail:function(data){
-                alert("system error");
+            error: function(data) {
+                alert("init data fail");
             }
         });
     }
 
     //filter item
-    function filterPlace(items,filterWord){
+    function filterPlace(items, filterWord) {
 
     }
 
     //main model function
     function MapViewModel() {
-        var _=this;
+        var _ = this;
         //toggle menu function use true and false to control the menu style
         _.showMenu = ko.observable(true);
         //search keyword
-        _.searchWord= ko.observable("");
+        _.searchWord = ko.observable("");
 
         //after search items
-        _.searchItems=ko.observableArray([]);
+        _.searchItems = ko.observableArray([]);
 
         //init search type
-        _.type=ko.observable("store");
+        _.type = ko.observable("store");
 
-        _.toggleMenu = function(){
+        _.toggleMenu = function() {
             _.showMenu(!this.showMenu());
         };
 
         //init data
         request = {
-            "action":"s",
+            "action": "s",
             "location": "San fransaco",
             "limit": 50
         };
-        searchPlace(request,function(data){
-            init=true;
-            places=data.businesses.map(function(i){
-                return new place(i,map);
+        searchPlace(request, function(data) {
+            init = true;
+            places = data.businesses.map(function(i) {
+                return new placeItem(i, map);
             });
 
             //init filter function after data is ready
             _.searchItems(places);
             ko.computed(function() {
                 var filter = _.searchWord().toLowerCase();
-                if (filter==="") {
-                    places.forEach(function(i){
+                if (filter === "") {
+                    places.forEach(function(i) {
                         i.show();
                     });
                     _.searchItems(places);
                 } else {
                     _.searchItems(ko.utils.arrayFilter(places, function(item) {
-                        if(item.data.name.toLowerCase().indexOf(filter) >= 0){
+                        if (item.data.name.toLowerCase().indexOf(filter) >= 0) {
                             item.show();
                             return true;
-                        }else{
+                        } else {
                             item.clear();
                             return false;
                         }
@@ -145,10 +145,9 @@ function initMap() {
                 }
             });
         });
-        
+
     }
 
     //bind with view
     ko.applyBindings(new MapViewModel());
 }
-
